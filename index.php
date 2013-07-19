f8b2520de80fa1b0c2653be3366b3763ff5d935c
Marin Salinas <m.mslns27@gmail.com>
2013-07-19 14:13:05 -0500

    enviando cosas

---
+++ index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>Marin Salinas</title>
+	<link href='http://fonts.googleapis.com/css?family=Fauna+One' rel='stylesheet' type='text/css'>
+	<link type="text/css" rel="stylesheet" href="stylesheet.css" />
+	<link rel="shortcut icon" href="img/favicon.ico" /> 
+</head>
+<body>
+		<header id="topBar">
+			<div id="nombretop"><a href="index.php" title="Página Web de Marín Salinas"><strong>@marinslns</strong></a></div>
+			<ul id="social">
+				<li id="facebook">
+					<a href="http://www.facebook.com/msalinass" title="Facebook: /msalinass" target="_blank"><span>/msalinass</span></a>
+				</li>
+				<li id="twitter">
+					<a href="http://www.twitter.com/marinslns" title="Twitter: @marinslns" target="_blank"><span>@marinslns</span></a>
+				</li>
+				<li id="gmail">
+					<a href="mailto:m.mslns27@gmail.com" title="Gmail: m.mslns27@gmail.com"><span>m.mslns27@gmail.com</span></a>
+				</li>
+				<li id="github">
+					<a href="http://www.github.com/mslns27" title="Github: /mslns27" target="_blank"><span>/mslns27</span></a>
+				</li>
+			</ul>
+		</header>
+		<section>
+		<div id="contenedor2">
+			<header id="encabezado">
+				<div id="perfil"></div>
+				<hgroup >
+				<!--<img id="perfil" src="img/perfil.jpg" alt="Imagen de Perfil de Miguel Marína Salinas Sánchez" />-->
+					<h1>Miguel Mar&iacute;n Salinas S&aacute;nchez</h1>
+					<h2>
+						Soy estudiante de la carrera <strong>Ingeniero en Tecnología de Software</strong>, 
+						<a href="http://www.fime.uanl.mx" target="_blank" title="http://www.fime.uanl.mx"><strong>FIME</strong></a> de la 
+						<a href="http://www.uanl.mx" target="_blank" title="http://www.uanl.mx"><strong>UANL</strong></a>.
+					</h2>
+				</hgroup>
+				<p>Relacionado con tecnologías como <strong>PHP, HTML, CSS, JavaScript, Python, Java</strong> interesado en el <strong>desarrollo web</strong>, así como las nuevas tecnologías móviles y embebidos.<p>
+				<hr/>
+			</header>
+			<nav id="navi">
+				<ul>
+					<li>Datos Personales</li>
+					<li>Estudios</li>
+					<li>Experiencia</li>
+					<li>Conocimientos afines a TI</li>					
+					</ul>
+			</nav>	
+				<div id='textPr'>
+					<article>
+						<hgroup>
+							<h1>Bienvenido a mis Datos Personales</h1>
+						</hgroup>
+						<p>
+						  Bitters bespoke iPhone retro vegan. Cosby sweater vegan irony keffiyeh bitters. Chillwave hella Carles brunch tofu. Brnch cray iPhone, letterpress semiotics PBR banjo sustainable squid authentic mlkshk. Ugh pitchfork VHS flannel, salvia aesthetic jean shorts mlkshk YOLO Echo Park freegan post-ironic. Umami next level Truffaut blog, sriracha letterpress Williamsburg. Small batch gentrify farm-to-table bicycle rights keytar pitchfork.
+						</p>
+					</article>
+					<aside>
+						<p>Correo Electrónico</p>
+						<p>Facebook</p>
+						<p>Twitter</p>
+					</aside>
+				</div>
+			</div>	
+		</section>
+	<footer>
+		<p><small>2013 - Propiedad de Marin Salinas</small></p>
+		<p><small><?php echo "Última actualización: " . date ("F d, Y.", getlastmod());?></small>
+	</footer>
+</body>
+</html>
